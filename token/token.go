4ed4b0fe420b79837752ce7bd644d62f2275a823
Patrick Smith <smithpatrick1992@gmail.com>
2025-08-22 17:09:57 -0400

    Moved literals and types in token files

--- token/token.go
+++ token/token.go
@@ -7,18 +7,6 @@ type Token struct {
 	Literal string
 }
 
-var keywords = map[string]TokenType{
-	"fn":  FUNCTION,
-	"let": LET,
-}
-
-func LookupIdent(ident string) TokenType {
-	if tok, ok := keywords[ident]; ok {
-		return tok
-	}
-	return IDENT
-}
-
 const (
 	ILLEGAL = "ILLEGAL"
 	EOF     = "EOF"
@@ -44,3 +32,15 @@ const (
 	FUNCTION = "FUNCTION"
 	LET      = "LET"
 )
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
