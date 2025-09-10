0a7a9f3399f4cbc06fbb717454afc8b51738f00f
patrick-j-smith <smithpatrick1992@gmail.com>
2025-09-10 01:29:00 -0400

    Added psedocode to parser need to convert to go functions

--- parser/parser.go
+++ parser/parser.go
@@ -29,3 +29,82 @@ func (p *Parser) nextToken() {
 func (p *Parser) ParseProgram() *ast.Program {
 	return nil
 }
+
+function parseProgram() {
+	program = newProgramASTNode()
+
+	advanceTokens()
+
+	for (currentToken() != EOF_TOKEN) {
+		statement = null
+
+		if (currentToken() == LET_TOKEN) {
+			statement = parseLetStatement()
+		} else if (currentToken() == RETURN_TOKEN) {
+			statement = parseReturnStatement()
+		} else if (currentToken() == EQUAL_TOKEN) {
+			statement = parseIfStatement()
+		}
+
+		if (statement != nil) {
+			program.Statements.push(statement)
+		}
+
+		advanceTokens()
+	}
+
+	return program
+}
+
+function parseLetStatement() {
+	advanceTokens()
+
+	identifier = parseIdentifier()
+
+	advanceTokens()
+
+	if currentToken() != EQUAL_TOKEN {
+		parseError("no equal sign!")
+		return null
+	}
+
+	advanceTokens()
+
+	value = parseExpression()
+
+	variableStatement = newVariableStatementASTNode()
+	variableStatement.identifier = identifier
+	variableStatement.expression = value
+	return variableStatement
+}
+
+function parseIdentifier() {
+	identifier = newIdentifierASTNode()
+	identifier.token = currentToken()
+	return identifier
+}
+
+function parseExpression() {
+	if (currentToken() == INTEGER_TOKEN) {
+		if (nextToken() == PLUS_TOKEN) {
+			return parseOperatorExpression()
+		} else if (nextToken() == SEMICOLON_TOKEN) {
+			return parseIntegerLiteral()
+		}
+	} else if (currentToken() == LEFT_PAREN) {
+		return parseGroupedExpression()
+	}
+}
+
+function parseOperatorExpression() {
+	operatorExpression = newOperatorExpression()
+
+	operatorExpression.left = parseIntergerLiteral()
+	advanceTokens()
+	operatorExpression.operator = currentToken()
+	advanceTokens()
+	operatorExpression.right = parseIntegerLiteral()
+
+	return operatorExpression
+}
+
