ce3d6a650673eba41f310fc09e5ef06fab2bf012
Patrick Smith <smithpatrick1992@gmail.com>
2025-09-10 20:46:25 -0400

    Adding parser testing

---
+++ parser/parser_test.go
@@ -0,0 +1,19 @@
+package parser
+
+import (
+	"testing"
+	"interpreter/ast"
+	"interpreter/lexer"
+)
+
+func TestLetStatements(t *testing.T) {
+	input := `
+		let x = 5;
+		let y = 10;
+		let foobar = 838383;
+	`
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+
+}
